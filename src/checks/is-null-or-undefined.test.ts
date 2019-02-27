a4cb9b3dfbc4af617928ead5feff79e390bb8cfe
LorenzoKoundouris <lorenzo.koundouris@gmail.com>
2019-02-27 23:45:17 +0000

    add test for isNullOrUndefined

---
+++ src/checks/is-null-or-undefined.test.ts
@@ -0,0 +1,37 @@
+import "jest";
+import { isNullOrUndefined } from "./is-null-or-undefined";
+
+describe("Checks whether a variable's value equals null or undefined", () => {
+    it("should detect null", () => {
+        const nullDetected = isNullOrUndefined(null);
+        expect(nullDetected).toBe(true);
+    });
+
+    it("should detect undefined", () => {
+        const undefinedDetected = isNullOrUndefined(undefined);
+        expect(undefinedDetected).toBe(true);
+    });
+
+    it("should not detect either", () => {
+        const valuesToTest = [
+            "test string",
+            "",
+            1337,
+            -1,
+            0,
+            [1, 2],
+            [],
+            {},
+            { foo: "bar" },
+            true,
+            false,
+            (): void => {},
+        ];
+        expect.assertions(valuesToTest.length);
+
+        valuesToTest.forEach((value) => {
+            const nullOrUndefinedDetected = isNullOrUndefined(value);
+            expect(nullOrUndefinedDetected).toBe(false);
+        });
+    });
+});

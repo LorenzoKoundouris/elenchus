5b44537e36940fe37d7997b078a961ca57a412e4
LorenzoKoundouris <lorenzo.koundouris@gmail.com>
2019-02-28 23:04:37 +0000

    test ci corrections

--- .deploy/bump-version.sh
+++ .deploy/bump-version.sh
@@ -17,15 +17,26 @@ PACKAGE_VERSION=$(node -p "require('./package.json').version")
 
 echo "PACKAGE_VERSION=${PACKAGE_VERSION}"
 
+BUMPED_PACKAGE_VERSION=$(cat ./package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g' \
+    | awk -F '.' '{ print $1,$2,$3+1 }' \
+    | sed 's/^ //' \
+| sed 's/ /./g')
+
+echo "BUMPED_PACKAGE_VERSION=${BUMPED_PACKAGE_VERSION}"
+
 if [ "$PREVIOUS_PACKAGE_VERSION" == "$PACKAGE_VERSION" ]
 then
-    npm version patch -m "CircleCi has bumped the patch version to $BUMPED_PACKAGE_VERSION [ci skip]" && git push -f
+    npm version patch --force -m "CircleCi has bumped the patch version to $BUMPED_PACKAGE_VERSION [ci skip]" && git push
     echo "Version bumped from the old one"
 elif [ "$PREVIOUS_PACKAGE_VERSION" == "" ]
 then
-    npm version patch -m "CircleCi has bumped the patch version to $BUMPED_PACKAGE_VERSION [ci skip]" && git push -f
+    npm version patch --force -m "CircleCi has bumped the patch version to $BUMPED_PACKAGE_VERSION [ci skip]" && git push
     echo "Version bumped wasn't able to find the previous one"
 else
-    git commit --allow-empty -m "CircleCi has released a specific version $PACKAGE_VERSION [ci skip]" && git push -f
+    git commit --allow-empty -m "CircleCi has released a specific version $PACKAGE_VERSION [ci skip]" && git push --tags
     echo "Version wasn't bumped due to a modification of the major or minor version"
 fi

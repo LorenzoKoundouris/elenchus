e0a057f3c35b841f98e2985ac1c78d9c0806fbc7
LorenzoKoundouris <lorenzo.koundouris@gmail.com>
2019-03-02 12:28:54 +0000

    use machine user rather than access token

--- .deploy/bump-version.sh
+++ .deploy/bump-version.sh
@@ -2,8 +2,8 @@
 
 # Description: bump version only if the previous master version was the same (not manually modified)
 # Requirements: The local package.json must have an upper property "url" setted with the git url of the project
-git config --global user.email $EMAIL_ADDR && git config --global user.name "LorenzoKoundouris" && git config --global push.default simple
-echo $EMAIL_ADDR
+git config --global user.email "bihlis91@gmail.com" && git config --global user.name "SlaveGit" && git config --global push.default simple
+
 git fetch
 git checkout HEAD~1
 
